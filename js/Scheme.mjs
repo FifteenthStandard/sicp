0f8a1cf77be00ff0051253ea262c22531a4a0943
Curtis Lusmore <curtis@fifteenthstandard.com>
2023-05-06 22:09:32 +1000

    Refactor to parsing

--- js/Scheme.mjs
+++ js/Scheme.mjs
@@ -1,55 +1,59 @@
 function parse(source) {
   const expressions = [];
   while (true) {
-    const { end, endOfList, error, result, continueFrom } = parseNext(source);
-    if (end) return expressions;
-    if (error) throw new Error(error);
-    if (endOfList) throw new Error("Error while parsing expression: Unexpected end of list marker ')'");
-    if (result) expressions.push(result);
+    const { result, error, value, continueFrom } = parseNext(source);
+    switch (result) {
+      case 'endOfFile': return expressions;
+      case 'endOfList': throw new Error("Error while parsing expression: Unexpected end of list marker ')'");
+      case 'error': throw new Error(error);
+      case 'success': expressions.push(value);
+    }
     source = continueFrom;
   }
 };
 
 function parseNext(source) {
   source = source.trimStart();
-  if (!source) return { end: true };
+  if (!source) return { result: 'endOfFile' };
   if (source[0] === ';') return parseComment(source);
   if (source[0] === '(') return parseList(source);
-  if (source[0] === ')') return { endOfList: true, continueFrom: source.substring(1) };
+  if (source[0] === ')') return { result: 'endOfList', continueFrom: source.substring(1) };
   return parseAtom(source);
 };
 
 function parseComment(source) {
-  if (source[0] !== ';') return { error: "Internal error: Expected start of comment marker ';'" };
+  if (source[0] !== ';') return { result: 'error', error: "Internal error: Expected start of comment marker ';'" };
   source = source.substring(2); // skip '; '
   let endOfLine = source.indexOf('\n');
   if (endOfLine === -1) endOfLine = source.length;
   const comment = source.substring(0, endOfLine);
-  return { comment, continueFrom: source.substring(endOfLine) };
+  return { result: 'comment', comment, continueFrom: source.substring(endOfLine) };
 };
 
 function parseList(source) {
-  if (source[0] !== '(') return { error: "Internal error: Expected start of list marker '('" };
+  if (source[0] !== '(') return { result: 'error', error: "Internal error: Expected start of list marker '('" };
   source = source.substring(1);
   const elems = [];
   while (true) {
-    const { end, endOfList, error, result, continueFrom } = parseNext(source);
-    if (error) return { error: `Error while parsing list: ${error}` };
-    if (end) return { error: "Error while parsing list: Unexpected end of input" };
-    if (endOfList) return { result: elems, continueFrom };
-    if (result) elems.push(result);
+    const { result, error, value, continueFrom } = parseNext(source);
+    switch (result) {
+      case 'endOfFile': return { result: 'error', error: "Error while parsing list: Unexpected end of input" };
+      case 'endOfList': return { result: 'success', value: elems, continueFrom };
+      case 'error': return { result: 'error', error: `Error while parsing list: ${error}` };
+      case 'success': elems.push(value);
+    }
     source = continueFrom;
   }
 };
 
 function parseAtom(source) {
   source = source.trimStart();
-  if (!source) return { error: "Internal error: Unexpected end of input while parsing atom" };
-  if (source[0] === '(') return { error: "Internal error: Unexpected start of list marker '(' while parsing atom" };
-  if (source[0] === '(') return { error: "Internal error: Unexpected end of list marker ')' while parsing atom" };
-  const result = source.match(/^[^\s\(\)]+/)[0];
-  const continueFrom = source.substring(result.length);
-  return { result, continueFrom };
+  if (!source) return { result: 'error', error: "Internal error: Unexpected end of input while parsing atom" };
+  if (source[0] === '(') return { result: 'error', error: "Internal error: Unexpected start of list marker '(' while parsing atom" };
+  if (source[0] === '(') return { result: 'error', error: "Internal error: Unexpected end of list marker ')' while parsing atom" };
+  const value = source.match(/^[^\s\(\)]+/)[0];
+  const continueFrom = source.substring(value.length);
+  return { result: 'success', value, continueFrom };
 };
 
 function evaluate(source) {
